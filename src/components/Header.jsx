f926846107bc53de227346f29648b6c330dd6370
Amer Melhem <amermelhem.99@icloud.com>
2025-09-02 15:10:42 +0300

    cart context

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,85 @@
 import { FaShoppingCart } from "react-icons/fa";
+import { MdOutlineDeleteOutline } from "react-icons/md";
+import { AiOutlineDelete } from "react-icons/ai";
+
 import { useCart } from "../context/CartContext";
+import { useState } from "react";
 const Header = () => {
-  const { cart } = useCart();
+  const [showDropDown, setShowDropDown] = useState(false);
+
+  const { cart, removeFromCart, clearCart } = useCart();
   const itemCount = cart.reduce((acc, item) => acc + item.qty, 0);
+  const total = cart
+    .reduce((acc, item) => acc + item.price * item.qty, 0)
+    .toFixed(2);
+
   return (
     <header className="bg-white shadow-md p-4 flex justify-between items-center">
       <h1 className="text-2xl font-bold text-blue-600">My Market</h1>
       <div className="relative">
-        <FaShoppingCart className="text-2xl text-gray-600" />
-        {itemCount > 0 && (
-          <span
-            className="absolute -top-2 -right-2 bg-red-500 text-white
+        <button
+          className="cursor-pointer"
+          onClick={() => setShowDropDown(!showDropDown)}
+        >
+          <FaShoppingCart className="text-2xl text-gray-600" />
+          {itemCount > 0 && (
+            <span
+              className="absolute -top-2 -right-2 bg-red-500 text-white
           text-xs w-5 h-5 flex items-center justify-center rounded-full"
-          >
-            {itemCount}
-          </span>
+            >
+              {itemCount}
+            </span>
+          )}
+        </button>
+        {showDropDown && (
+          <div className="absolute right-0 mt-2 w-80 bg-white border rounded shadow-lg z-50">
+            <div className="p-4">
+              <h2 className="font-semibold text-lg mb-2">Cart Items</h2>
+              <hr className="border-t-2 border-blue-300 mb-3" />
+
+              {cart.length === 0 ? (
+                <p className="text-gray-500 text-sm">Your cart is empty</p>
+              ) : (
+                <>
+                  <ul className="max-h-60 overflow-y-auto divide-y divide-gray-200">
+                    {cart.map((item) => (
+                      <li
+                        className="flex justify-between items-center py-2"
+                        key={item.id}
+                      >
+                        <div>
+                          <p className="font-semibold">{item.name}</p>
+                          <p className="text-sm text-gray-500">
+                            {item.qty} x ${item.price}
+                          </p>
+                        </div>
+                        <button
+                          className="text-red-500 text-lg hover:cursor-pointer hover:scale-110 transition-transform "
+                          onClick={() => removeFromCart(item.id)}
+                        >
+                          <MdOutlineDeleteOutline />
+                        </button>
+                      </li>
+                    ))}
+                  </ul>
+                  <hr className="border-t-2 border-blue-300 mt-4 mb-2" />
+                  <div className="mt-4 flex justify-between font-semibold">
+                    <span>Total: </span>
+                    <span>${total}</span>
+                  </div>
+                  <button
+                    onClick={clearCart}
+                    className="mt-3 w-full bg-red-500 text-white
+                               py-1 rounded transition 
+                               hover:bg-red-600 flex items-center justify-center gap-2"
+                  >
+                    <AiOutlineDelete className="hover:scale-110 transition-transform" />
+                    Clear Cart
+                  </button>
+                </>
+              )}
+            </div>
+          </div>
         )}
       </div>
     </header>
